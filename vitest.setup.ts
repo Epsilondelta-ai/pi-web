2cbf39d57891d83696dc55b25959b5caeb515791
juunini <juuni.ni.i@gmail.com>
2026-05-29 11:04:02 +0900

    Polyfill DOM ranges for CodeMirror tests
    Co-authored-by: JuunAI <juunai.ai.i@gmail.com>

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,3 +1,26 @@
 import "@testing-library/jest-dom/vitest";
 
 (globalThis as any).IS_REACT_ACT_ENVIRONMENT = true;
+
+const emptyDomRectList = () => ({
+  length: 0,
+  item: () => null,
+  [Symbol.iterator]: function* () {},
+}) as DOMRectList;
+
+if (typeof Range !== "undefined" && !Range.prototype.getClientRects) {
+  Range.prototype.getClientRects = emptyDomRectList;
+}
+if (typeof Range !== "undefined" && !Range.prototype.getBoundingClientRect) {
+  Range.prototype.getBoundingClientRect = () => ({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    toJSON: () => ({}),
+  }) as DOMRect;
+}
